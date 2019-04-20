0e24e6090f440783af0518e806c0b047e135c401
SimplyRin <simplyrin.dev@gmail.com>
2019-04-20 21:56:32 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# ProcessorInfo
+PC 情報を確認することができるプラグイン。
+
+Spigot, BungeeCord に対応してます。
+
+![b56c983d8e4be50614538411a3463dc5](https://i.gyazo.com/b56c983d8e4be50614538411a3463dc5.png "b56c983d8e4be50614538411a3463dc5")
+
+# Download
+- [ProcessorInfo-1.0.jar](https://github.com/SimplyRin/ProcessorInfo/releases/download/1.0/ProcessorInfo-1.0.jar)
+
+# Open Source Lisence
+- oshi
+  - Lisence: The MIT Lisence (MIT)
+  - Copyright (c) 2010 - 2019 The OSHI Project Team:
+    - https://github.com/oshi/oshi/graphs/contributors
+  - https://github.com/oshi/oshi/blob/master/LICENSE_HEADER
---
+++ src/main/java/net/simplyrin/processorinfo/spigot/command/CommandProcessorInfo.java
@@ -0,0 +1,58 @@
+package net.simplyrin.processorinfo.spigot.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import net.simplyrin.processorinfo.spigot.Main;
+import oshi.SystemInfo;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.util.FormatUtil;
+
+/**
+ * Created by SimplyRin on 2019/04/20.
+ *
+ * Copyright (c) 2019 SimplyRin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+public class CommandProcessorInfo implements CommandExecutor {
+
+	private Main instance;
+
+	public CommandProcessorInfo(Main instance) {
+		this.instance = instance;
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (!sender.hasPermission("processorinfo.show")) {
+			this.instance.sendMessage(sender, "&cYou don't have access to this command!");
+			return true;
+		}
+
+		SystemInfo systemInfo = this.instance.getSystemInfo();
+		HardwareAbstractionLayer hardware = systemInfo.getHardware();
+
+		this.instance.sendMessage(sender, "&aOS: " + systemInfo.getOperatingSystem());
+		this.instance.sendMessage(sender, "&aCPU: " + hardware.getProcessor());
+		this.instance.sendMessage(sender, "&aMemory: " + FormatUtil.formatBytes(hardware.getMemory().getTotal()));
+		return true;
+	}
+
+}
